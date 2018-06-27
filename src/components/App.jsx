3d4f87654b9c52ead5fd6c0b1bbdc75b24500913
Kristi <krsy3ii@yahoo.com>
2018-06-27 15:01:32 -0700

    set up environment with starter components and initial lint run returning clean

---
+++ src/components/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+// import Header from './Header'
+// import CropsList from './CropsList'
+// import LocationsList from './LocationsList'
+// import { Switch, Route } from 'react-router-dom'
+
+function App(){
+  return (
+    <div>
+      <h1>Avery Farms</h1>
+      <h3>View Updates</h3>
+      <h5>Seasonal Crops</h5>
+      <h5>Farmer's Market Locations</h5>
+      <p><em>list</em></p>
+    </div>
+  )
+}
+
+export default App
